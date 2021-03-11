3bfd870a07a396c14c3066eb8420cfa65c70beaf
anpopov <anpopov@at-consulting.ru>
2021-03-11 12:25:37 +0300

    Add simple get example

---
+++ open-borders-main/src/main/java/space/ffisherr/openborders/controller/MainChatController.java
@@ -0,0 +1,16 @@
+package space.ffisherr.openborders.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/chat/")
+public class MainChatController {
+
+    @GetMapping
+    public String greeting () {
+        return "Hello";
+    }
+
+}
