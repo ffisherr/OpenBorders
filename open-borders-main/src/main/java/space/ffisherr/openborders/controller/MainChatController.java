d16bae88c1ae8206b668602005d713e582fa23d9
Alexey <wzor101@mail.ru>
2021-03-11 13:42:12 +0300

    Добавил Post

--- open-borders-main/src/main/java/space/ffisherr/openborders/controller/MainChatController.java
+++ open-borders-main/src/main/java/space/ffisherr/openborders/controller/MainChatController.java
@@ -1,6 +1,7 @@
 package space.ffisherr.openborders.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,11 @@ public class MainChatController {
     @GetMapping
     public String greeting () {
         return "Hello";
+    }
+
+    @PostMapping
+    public String posting (){
+        return "Post";
     }
 
 }
